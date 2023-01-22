ebafa062d79139aff237821894b8b1c851439df8
Muhammad Abdullah Nasir <114460800+daredevilx616@users.noreply.github.com>
2023-01-22 14:44:00 -0500

    Update twi_keys.py

--- twi_keys.py
+++ twi_keys.py
@@ -2,4 +2,4 @@ api = 'XXX' #Use your own Twitter API keys
 apiSecret = 'XXX'
 bearerToken = 'XXX'
 accessToken = 'XYZ'
-accessTokenSecret = '3eoJVUwwk1ptZJJ6hxL3SVUImRjk4NLiHuqI537vn4urH'
+accessTokenSecret = 'ABC'
